9769c5fdf1604f075e28810be0ffc4c01b7a3181
fistadev <fabiofistarol@gmail.com>
2021-08-19 13:07:39 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-    path('wordcloud/', include('wordcloud.urls')),
+    # path('wordcloud/', include('wordcloud.urls')),
 ]
